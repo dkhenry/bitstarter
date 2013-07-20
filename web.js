6f98a8ff60321d1068ff2c6f002520300e8339da
Dan Kozlowski <dan.kozlowski@gmail.com>
2013-07-19 21:36:04 -0400

    Added the marketing materials

--- web.js
+++ web.js
@@ -9,6 +9,8 @@ app.get('/', function(request, response) {
 	});
 });
 
+app.use('/static', express.static(__dirname + '/public'));
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
