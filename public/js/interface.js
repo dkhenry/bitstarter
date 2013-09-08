955c8cb5932b5edda9f8aab103587ad9a752f2b2
Dan Kozlowski <dan.kozlowski@gmail.com>
2013-09-07 20:34:02 -0400

    Fixed some chrome issues and fleshed out the rest of the MEAN stack

--- public/js/interface.js
+++ public/js/interface.js
@@ -1,15 +1,18 @@
 
 var mainScreen = angular.module('mainScreen', []);
 
-function mainCtrl($scope) {	
-var socket = io.connect(window.location.hostname);
-socket.on('heartbeat', function (data) {
-  console.log(data.clients);	  
-  $scope.userData = data;
-});	
-	
-}
 
-$(function() { 
+mainScreen.controller("MainCtrl",function($scope) {
+	$scope.userData = {
+			backers: 0,
+			total: 0,
+			days: 40
+			
+	}; 
 	
+	var socket = io.connect(window.location.hostname);
+	socket.on('heartbeat', function (data) {
+		console.log(data.clients);	  
+		$scope.userData.clients = data.clients;
+	});	
 });
